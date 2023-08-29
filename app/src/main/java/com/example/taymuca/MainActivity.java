57cd36ee34fefd5ffc212d465fde69805763a441
IPreHandaI <tayrelpreci@hotmail.com>
2023-08-29 08:54:59 -0300

    commit 0.1
    primeira link ODS-1

--- app/src/main/java/com/example/taymuca/MainActivity.java
+++ app/src/main/java/com/example/taymuca/MainActivity.java
@@ -10,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        public Button BtnOds1, BtnOds2, BtnOds3, BtnMain;
+
+        BtnOds1 = findViewById(R.id.BtnOds1);
+        BtnOds1.setOnClickListener(new View.OnClickListener()) {
+            @Override
+                    public void OnClick(View View){
+                Intent intent = new Intent(getApplicationContext(), ActivityOds1.class);
+                startActivity(intent);
+            }
+
+        }
+
+
     }
 }

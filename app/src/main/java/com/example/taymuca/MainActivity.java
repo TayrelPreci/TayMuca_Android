20dec464ff9b8ef583c3669c2b3d9a0a0c518c77
Tayrel Preci <tayrelpreci@hotmail.com>
2023-09-04 09:34:38 -0300

    Commit 0.4
    Mais um ATT

--- app/src/main/java/com/example/taymuca/MainActivity.java
+++ app/src/main/java/com/example/taymuca/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.taymuca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        public Button BtnOds1, BtnOds2, BtnOds3, BtnMain;
+        ImageButton BtnOds1 = findViewById(R.id.BtnOds1);
 
-        BtnOds1 = findViewById(R.id.BtnOds1);
-        BtnOds1.setOnClickListener(new View.OnClickListener()) {
+        return BtnOds1.setOnClickListener(new View.OnClickListener() {
             @Override
-                    public void OnClick(View View){
+            public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), ActivityOds1.class);
                 startActivity(intent);
             }
